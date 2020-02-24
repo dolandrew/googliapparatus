52d8a326febfac3188650d964988c246c5eda439
Andrew Dolan <andrew.dolan@skyward.io>
2020-02-23 19:33:52 -0800

    Add endpoint for querying songs by lyrics containing a term, and updated README with build instructions

--- src/main/java/googliapparatus/controller/SearchController.java
+++ src/main/java/googliapparatus/controller/SearchController.java
@@ -1,14 +1,34 @@
 package googliapparatus.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import googliapparatus.SongEntity;
+import googliapparatus.dto.SongDTO;
+import googliapparatus.repository.SongEntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/search")
+@CrossOrigin
 public class SearchController {
-    @GetMapping
-    public String helloWorld() {
-        return "hello world";
+
+    @Autowired
+    private SongEntityRepository songEntityRepository;
+
+    @GetMapping("/lyrics")
+    public List<SongDTO> searchLyrics(String filter) {
+        List<SongEntity> songEntities = songEntityRepository.findByLyricsContains(filter);
+        List<SongDTO> songs = new ArrayList<>();
+        for (SongEntity songEntity : songEntities) {
+            SongDTO songDTO = new ObjectMapper().convertValue(songEntity, SongDTO.class);
+            songs.add(songDTO);
+        }
+        return songs;
     }
 }
