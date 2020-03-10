dd64ef9106e42682837fa3becb00101dfcc9b187
Andrew Dolan <andrew.dolan@skyward.io>
2020-03-10 12:24:14 -0700

    Return lyric snippets with results, created SnippetHelper, added search by song name, updated tests

--- src/main/java/googliapparatus/dto/SongDTO.java
+++ src/main/java/googliapparatus/dto/SongDTO.java
@@ -2,11 +2,16 @@ package googliapparatus.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SongDTO {
     private String name;
 
     private String link;
+
+    private Set<String> lyricSnippets = new HashSet<>();
 
     public String getName() {
         return name;
@@ -22,5 +27,13 @@ public class SongDTO {
 
     public void setLink(String link) {
         this.link = link;
+    }
+
+    public Set<String> getLyricSnippets() {
+        return lyricSnippets;
+    }
+
+    public void setLyricSnippets(Set<String> lyricSnippets) {
+        this.lyricSnippets = lyricSnippets;
     }
 }
---
+++ src/main/java/googliapparatus/helper/SnippetHelper.java
@@ -0,0 +1,39 @@
+package googliapparatus.helper;
+
+import googliapparatus.dto.SongDTO;
+import googliapparatus.entity.SongEntity;
+
+public class SnippetHelper {
+
+    public void findRelevantLyrics(String filter, SongEntity songEntity, SongDTO songDTO) {
+        String lyrics = songEntity.getLyrics();
+        if (lyrics != null) {
+            int index = lyrics.indexOf(filter);
+            int snippetLength = 20;
+            while (index >= 0) {
+                String snippet = getSnippet(filter, lyrics, index, snippetLength);
+                snippet = capitalize(snippet);
+                snippet = highlightFilter(filter, snippet);
+                songDTO.getLyricSnippets().add(snippet);
+                index = getNextFilterOccurencce(filter, lyrics, index);
+            }
+        }
+    }
+
+    private String getSnippet(String filter, String lyrics, int index, int snippetLength) {
+        return lyrics.substring(Math.max(0, index - snippetLength),
+                Math.min(filter.length() + index + snippetLength, lyrics.length())) + "...";
+    }
+
+    private int getNextFilterOccurencce(String filter, String lyrics, int index) {
+        return lyrics.indexOf(filter, index+1);
+    }
+
+    private String capitalize(String snippet) {
+        return snippet.replace(" i ", " I ");
+    }
+
+    private String highlightFilter(String filter, String snippet) {
+        return snippet.replace(filter, "<b>" + filter + "</b>");
+    }
+}
